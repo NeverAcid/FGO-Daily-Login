eb4a3047d0cf211d0bd79b4de81d5c3df9831eaa
NeverAcid <146020489+NeverAcid@users.noreply.github.com>
2025-06-12 05:41:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -33,12 +33,6 @@ coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 def check_blue_apple_cron(instance):
     if blue_apple_cron:
-
-        cron = croniter(blue_apple_cron)
-        next_date = cron.get_next(datetime)
-        current_date = datetime.now()
-
-        if current_date >= next_date:
             logger.info('Trying buy one blue apple!')
             instance.buyBlueApple(1)
             time.sleep(2)
